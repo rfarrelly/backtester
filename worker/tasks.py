90c324067a11fe1782ce6581de8356fa52aeea49
Ryan Farrelly <rfarrelly03@qub.ac.uk>
2026-02-19 11:29:45 +0000

    Initial commit of backtester. Docker and skeleton project.

---
+++ worker/tasks.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+celery_app = Celery(
+    "worker",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0",
+)
+
+
+@celery_app.task
+def test_task():
+    return "Worker is alive"
